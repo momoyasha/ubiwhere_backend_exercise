e717d0050419b4a6b3c2a8a9bd65614b4d096585
momoyasha <matheusc.oliveira@live.com>
2025-07-21 19:58:46 +0100

    remove separate schema reference

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,7 +83,6 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "SCHEMA": "backend_exercise",
         "HOST": "db",
         "PORT": "5432",
     }
