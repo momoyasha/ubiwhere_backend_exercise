f5715a43dc19f0d37e649a02a190a59364664a17
momoyasha <matheusc.oliveira@live.com>
2025-07-21 20:51:20 +0100

    create speed_measurement_repository

---
+++ backend/api/repository/speed_measurement_repository.py
@@ -0,0 +1,25 @@
+from api.models.speed_measurement import SpeedMeasurement
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class SpeedMeasurementRepository:
+    """
+    Classe de métodos para interagir com entradas SpeedMeasurement
+    da base de dados.
+    """
+
+    @staticmethod
+    def save_new_speed_measurement(speed_measurement_obj: SpeedMeasurement):
+        """
+        Salva um novo objeto SpeedMeasurement na base de dados.
+        """
+        try:
+            speed_measurement_obj.save()
+            speed_measurement_obj.refresh_from_db()
+            return speed_measurement_obj
+        except Exception as ex:
+            logger.error(f"Erro ao salvar SpeedMeasurement - [{ex}]")
+            return None
