51b88a897c79dc0460082ff5ace04b50c8ed3652
momoyasha <matheusc.oliveira@live.com>
2025-07-21 21:50:50 +0100

    create endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
 from rest_framework.routers import DefaultRouter
-
+from api.views.speed_measurement_views import SpeedMeasurementViewSet
 
 router = DefaultRouter()
+
+router.register(
+    r"speed_measurements", SpeedMeasurementViewSet, basename="speed_measurement"
+)
 
 urlpatterns = router.urls
