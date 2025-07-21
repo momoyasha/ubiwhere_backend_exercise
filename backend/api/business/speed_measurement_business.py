2a54b7e4c3ade326a02a0f1efb3dc3ac8f0538fc
momoyasha <matheusc.oliveira@live.com>
2025-07-21 20:51:38 +0100

    create speed_measurement_business

---
+++ backend/api/business/speed_measurement_business.py
@@ -0,0 +1,31 @@
+from api.models.speed_measurement import SpeedMeasurement
+import csv
+from django.conf import settings
+from django.contrib.gis.geos import Point
+
+
+class SpeedMeasurementBusiness:
+    """
+    Classe de métodos para tratar objetos SpeedMeasurement.
+    """
+
+    def populate_database_with_initial_data():
+        """
+        Carga inicial na base de dados a partir do csv fornecido no exercício.
+        """
+        data_points = []
+
+        file_path = settings.BASE_DIR / "data" / "traffic_speed.csv"
+
+        with open(file_path) as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                obj = SpeedMeasurement(
+                    measurement_id=row["ID"],
+                    start_point=Point(
+                        float(row["Long_start"]), float(row["Lat_start"])
+                    ),
+                    end_point=Point(float(row["Long_end"]), float(row["Lat_end"])),
+                    length=row["Length"],
+                    speed=float(row["Speed"]),
+                )
