ba95b8e8a167a73003e9cf9546a4f074c159d725
momoyasha <matheusc.oliveira@live.com>
2025-07-21 20:26:17 +0100

    add logging

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,3 +128,45 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+            "level": "INFO",
+        },
+        # "file": {
+        #     "class": "logging.FileHandler",
+        #     "filename": "django_app.log",
+        #     "formatter": "verbose",
+        #     "level": "DEBUG",
+        # },
+    },
+    "loggers": {
+        # logger padrão do Django
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+        # logger para sua app específica
+        "api": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
