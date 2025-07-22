0f7be78a9aaff07980d8c1f1c093449913f51044
momoyasha <matheusc.oliveira@live.com>
2025-07-22 13:43:39 +0100

    add to method to create speed interval

--- backend/api/repository/speed_interval_repository.py
+++ backend/api/repository/speed_interval_repository.py
@@ -12,37 +12,28 @@ class SpeedIntervalRepository:
     """
 
     @staticmethod
-    def save_speed_interval(criticality: int, min_speed=None, max_speed=None):
+    def save_speed_interval(
+        criticality: int, criticality_text: str, min_speed=None, max_speed=None
+    ):
         """
         Cadastra ou atualiza um intervalo de velocidade.
         """
         try:
-            existing_interval = SpeedInterval.objects.get(criticality=criticality)
+            existing_interval = SpeedInterval.objects.get(
+                criticality=criticality, criticality_text=criticality_text
+            )
         except SpeedInterval.DoesNotExist:
-            existing_interval = SpeedInterval(criticality=criticality)
+            existing_interval = SpeedInterval()
 
+        existing_interval.criticality = criticality
+        existing_interval.criticality_text = criticality_text
         existing_interval.max_speed = max_speed
         existing_interval.min_speed = min_speed
 
         try:
             existing_interval.save()
+            logger.info(
+                f"Intervalo de velocidade (min {min_speed}, max {max_speed}) criado"
+            )
         except Exception as ex:
             logger.error(f"Erro ao salvar intervalo de velocidade - [{ex}]")
-
-    @staticmethod
-    def check_speed_classification(speed: float):
-        """
-        Classifica a velocidade fornecida de acordo com os
-        intervalos de velocidade cadastrados.
-        """
-        speed_intervals = list(SpeedInterval.objects.all())
-
-        for interval in speed_intervals:
-            # se a velocidade é maior do que a mínima (se esta existir)
-            # e menor do que a máxima (se esta existir)
-            if (not interval.min_speed or speed > interval.min_speed) and (
-                not interval.max_speed or speed < interval.max_speed
-            ):
-                return interval
-
-        return None
