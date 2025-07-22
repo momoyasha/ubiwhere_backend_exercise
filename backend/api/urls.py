40eaac45449990d7349898fdbb2e9ec09227a86e
momoyasha <matheusc.oliveira@live.com>
2025-07-22 12:32:00 +0100

    include route segments endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,15 @@
 from rest_framework.routers import DefaultRouter
 from api.views.speed_measurement_views import SpeedMeasurementViewSet
+from api.views.road_segment_views import RoadSegmentViewSet
 
 router = DefaultRouter()
 
 router.register(
-    r"speed_measurements", SpeedMeasurementViewSet, basename="speed_measurement"
+    r"speed_measurements",
+    SpeedMeasurementViewSet,
+    basename="speed_measurement",
 )
+router.register(r"road_segments", RoadSegmentViewSet, basename="road_segment")
+
 
 urlpatterns = router.urls
