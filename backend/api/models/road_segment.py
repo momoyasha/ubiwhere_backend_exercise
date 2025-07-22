a7aad9e84f638b92a1b86f2dbc4fcee098f060df
momoyasha <matheusc.oliveira@live.com>
2025-07-22 10:55:54 +0100

    add road_segment as a standalone object

---
+++ backend/api/models/road_segment.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.gis.db.models import PointField
+
+
+class RoadSegment(models.Model):
+    """
+    Segmentos de estrada. Medições de velocidade são sempre
+    associadas a um desses.
+    """
+
+    start_point = PointField(null=False, blank=False)
+    end_point = PointField(null=False, blank=False)
+    length = models.FloatField(null=False, blank=False)
+    created_at = models.DateTimeField(blank=False, null=False, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=False, null=False, auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__} [{self.id}]"
+
+    constraints = [
+        models.UniqueConstraint(
+            fields=["start_point", "end_point"], name="unique_start_point_end_point"
+        )
+    ]
--- backend/api/models/speed_measurement.py
+++ backend/api/models/speed_measurement.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.gis.db.models import PointField
+from api.models.road_segment import RoadSegment
 
 
 class SpeedMeasurement(models.Model):
@@ -8,9 +9,10 @@ class SpeedMeasurement(models.Model):
     """
 
     measurement_id = models.IntegerField(null=False, blank=False, unique=True)
-    start_point = PointField(null=False, blank=False)
-    end_point = PointField(null=False, blank=False)
-    length = models.FloatField(null=False, blank=False)
+    road_segment = models.ForeignKey(RoadSegment, on_delete=models.CASCADE)
     speed = models.FloatField(null=False, blank=False)
     created_at = models.DateTimeField(blank=False, null=False, auto_now_add=True)
     updated_at = models.DateTimeField(blank=False, null=False, auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: road_segment: [{self.road_segment.id}]; speed:[{self.speed}]"
