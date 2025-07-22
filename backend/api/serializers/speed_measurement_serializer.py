8a0752e9feed669a6d1364e19938a8d545d02af7
momoyasha <matheusc.oliveira@live.com>
2025-07-22 11:41:31 +0100

    add road segment info to spd measurement serializer

--- backend/api/serializers/speed_measurement_serializer.py
+++ backend/api/serializers/speed_measurement_serializer.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 from rest_framework_gis.fields import GeometryField
 
 
-class SpeedMeasurementSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+class RoadSegmentSerializer(serializers.Serializer):
     start_point = GeometryField()
     end_point = GeometryField()
-    length = serializers.FloatField()
+
+
+class SpeedMeasurementSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    measurement_id = serializers.IntegerField()
+    road_segment = RoadSegmentSerializer()
     speed = serializers.FloatField()
     created_at = serializers.DateTimeField()
     updated_at = serializers.DateTimeField()
